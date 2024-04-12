d72d8529a7ac495d64745f397781d83cb75741bc
RickardEkstedt <ekstedtrickard@gmail.com>
2024-04-12 15:21:21 +0200

    Initial commit

---
+++ src/main/java/com/wigellkoncernen/biluthyrning/BiluthyrningApplication.java
@@ -0,0 +1,13 @@
+package com.wigellkoncernen.biluthyrning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiluthyrningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BiluthyrningApplication.class, args);
+    }
+
+}
