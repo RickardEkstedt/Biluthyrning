130c3d359b57c70c874e8a64f33b7dd178e26ad0
m4linolsson <malin.olsson.java23@edu.edugrade.se>
2024-04-17 12:06:45 +0200

    lagt till cancel order och log4jproperties med färg på text. Hoppas det funkar bättre nu

--- src/main/java/com/wigellkoncernen/biluthyrning/services/BookingService.java
+++ src/main/java/com/wigellkoncernen/biluthyrning/services/BookingService.java
@@ -1,4 +1,47 @@
 package com.wigellkoncernen.biluthyrning.services;
 
-public class BookingService {
+import com.wigellkoncernen.biluthyrning.entities.Booking;
+import com.wigellkoncernen.biluthyrning.entities.Customer;
+import com.wigellkoncernen.biluthyrning.repositories.BookingRepository;
+import com.wigellkoncernen.biluthyrning.repositories.exceptions.ResourceNotFoundException;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class BookingService implements BookingServiceInterface {
+    Logger logger = Logger.getLogger(BookingService.class);
+
+    @Autowired
+    BookingRepository bookingRepository;
+
+    @Override
+    public List<Booking> getBookings() {
+        return null;
+    }
+
+    @Override
+    public List<Booking> getMyBookings(Customer customer) {
+        return null;
+    }
+
+
+    @Override
+    public void cancelBooking(Booking booking) {
+        bookingRepository.findById(booking.getId()).orElseThrow(() -> new ResourceNotFoundException("Booking", "id", booking.getId()));
+//booking.setBooked(false);
+       // booking.setBooked(false);
+        bookingRepository.save(booking);
+        logger.log(Level.WARN, "Booking with id " + booking.getId() + " canceled");
+        //TODO kolla om det går att använda bara id och skicka med det från postman istället
+    }
+
+    @Override
+    public void deleteBooking(Booking booking) {
+
+    }
 }
